45cd1760a7e2e457084e982c9b89d1d208134870
katorie <katoriexxxkatorie@gmail.com>
2014-08-30 22:27:39 +0900

    generate cart with scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140830043201) do
+ActiveRecord::Schema.define(:version => 20140830132624) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
